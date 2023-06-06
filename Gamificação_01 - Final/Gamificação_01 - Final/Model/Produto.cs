33f974a2b0c90989a72efa3d0be8e03597a79d89
JoaoStrabelli <109425974+JoaoStrabelli@users.noreply.github.com>
2023-06-06 13:09:55 -0300

    Add files via upload

---
+++ Gamificação_01 - Final/Gamificação_01 - Final/Model/Produto.cs
@@ -0,0 +1,22 @@
+﻿using JRJ.Modas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace JRJ.Modas
+    {
+        public class ProdutoModel
+        {
+            public long? ProdutoID { get; set; }
+            public Guid ProdutoIDGUID { get; set; }
+            public string Descricao { get; set; } = string.Empty;
+            public string Nome { get; set; } = string.Empty;
+            public double Preco { get; set; }
+            public CategoriaModel? Categoria { get; set; }
+            public static readonly List<ProdutoModel> produtos = new List<ProdutoModel>();
+    }
+    }
+
